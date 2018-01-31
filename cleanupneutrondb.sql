4790932d177f29e2ae1eb2d9fb6bbb7bb03a911c
Shashank Kumar Shankar <shank7485@users.noreply.github.com>
2018-01-30 16:35:11 -0800

    Delete cleanupneutrondb.sql

--- cleanupneutrondb.sql
+++
@@ -1,9 +0,0 @@
-delete from lbaas_l7rules;
-delete from lbaas_l7policies;
-delete from lbaas_members;
-delete from lbaas_sessionpersistences;
-delete from lbaas_listeners;
-delete from lbaas_pools;
-delete from lbaas_healthmonitors;
-delete from lbaas_loadbalancer_statistics;
-delete from lbaas_loadbalancers;
